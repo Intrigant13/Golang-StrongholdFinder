58220fde534296fe8bc74edd3c528e5c554214c7
Intrigant13 <SintezPhosphor@gmail.com>
2024-08-06 17:31:13 +0700

    Changes:
    Interface adjustment for better usability.
    Added input validation checks.
    Added tooltips for input fields.

--- main.go
+++ main.go
@@ -13,7 +13,8 @@ import (
 func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Golang-StrongholdFinder by Intrigant13")
-	myWindow.Resize(fyne.NewSize(380, 160))
+	myWindow.Resize(fyne.NewSize(500, 200))
+	myWindow.SetFixedSize(true)
 	// Объявление некоторых переменных /Declaration of some variables
 	Z := ""
 	// Создание виджетов, кнопок и тд. /Creation of widgets, buttons, etc.
@@ -26,14 +27,42 @@ func main() {
 	EntryF2 := widget.NewEntry()
 	bt1 := widget.NewButton("Найти!", func() {
 		// Преобразование всех значений из str в float64 /Converting all values from str to float64
-		F1, _ := strconv.ParseFloat(EntryF1.Text, 64)
-		F2, _ := strconv.ParseFloat(EntryF2.Text, 64)
-		X1, _ := strconv.ParseFloat(EntryX1.Text, 64)
-		X2, _ := strconv.ParseFloat(EntryX2.Text, 64)
-		Z1, _ := strconv.ParseFloat(EntryZ1.Text, 64)
-		Z2, _ := strconv.ParseFloat(EntryZ2.Text, 64)
-		F1 = 3.141592653589793 * (0.5 + F1/180)
-		F2 = 3.141592653589793 * (0.5 + F2/180)
+		F1, err := strconv.ParseFloat(EntryF1.Text, 64)
+		if err != nil {
+			Out.SetText("Недопустимые символы F1")
+			return
+		}
+		F2, err := strconv.ParseFloat(EntryF2.Text, 64)
+		if err != nil {
+			Out.SetText("Недопустимые символы F2")
+			return
+		}
+		X1, err := strconv.ParseFloat(EntryX1.Text, 64)
+		if err != nil {
+			Out.SetText("Недопустимые символы X1")
+			return
+		}
+		X2, err := strconv.ParseFloat(EntryX2.Text, 64)
+		if err != nil {
+			Out.SetText("Недопустимые символы X2")
+			return
+		}
+		Z1, err := strconv.ParseFloat(EntryZ1.Text, 64)
+		if err != nil {
+			Out.SetText("Недопустимые символы Z1")
+			return
+		}
+		Z2, err := strconv.ParseFloat(EntryZ2.Text, 64)
+		if err != nil {
+			Out.SetText("Недопустимые символы Z2")
+			return
+		}
+		if F1 == F2 {
+			Out.SetText("Углы F1 и F2 равны\nВычисление невозможно")
+			return
+		}
+		F1 = math.Pi * (0.5 + F1/180)
+		F2 = math.Pi * (0.5 + F2/180)
 		// Основной код для расчета нахождение крепости с помощью триангуляции. /Main code for calculating the stronghold location using triangulation
 		findX := (math.Cos(F2)*(math.Cos(F1)*Z1-math.Sin(F1)*X1) - math.Cos(F1)*(math.Cos(F2)*Z2-math.Sin(F2)*X2)) / math.Sin(F2-F1)
 		findZ := (math.Sin(F2)*(math.Sin(F1)*X1-math.Cos(F1)*Z1) - math.Sin(F1)*(math.Sin(F2)*X2-math.Cos(F2)*Z2)) / math.Sin(F1-F2)
@@ -45,21 +74,29 @@ func main() {
 	Out.Move(fyne.NewPos(50, 100))
 	EntryX1.Move(fyne.NewPos(50, 20))
 	EntryX2.Move(fyne.NewPos(50, 60))
-	EntryZ1.Move(fyne.NewPos(150, 20))
-	EntryZ2.Move(fyne.NewPos(150, 60))
-	EntryF1.Move(fyne.NewPos(250, 20))
-	EntryF2.Move(fyne.NewPos(250, 60))
-	bt1.Move(fyne.NewPos(250, 100))
+	EntryZ1.Move(fyne.NewPos(200, 20))
+	EntryZ2.Move(fyne.NewPos(200, 60))
+	EntryF1.Move(fyne.NewPos(350, 20))
+	EntryF2.Move(fyne.NewPos(350, 60))
+	bt1.Move(fyne.NewPos(380, 100))
 
 	// Размер объектов /Size of objects
 	Out.Resize(fyne.NewSize(150, 25))
-	EntryX1.Resize(fyne.NewSize(70, 25))
-	EntryX2.Resize(fyne.NewSize(70, 25))
-	EntryZ1.Resize(fyne.NewSize(70, 25))
-	EntryZ2.Resize(fyne.NewSize(70, 25))
-	EntryF1.Resize(fyne.NewSize(70, 25))
-	EntryF2.Resize(fyne.NewSize(70, 25))
+	EntryX1.Resize(fyne.NewSize(100, 30))
+	EntryX2.Resize(fyne.NewSize(100, 30))
+	EntryZ1.Resize(fyne.NewSize(100, 30))
+	EntryZ2.Resize(fyne.NewSize(100, 30))
+	EntryF1.Resize(fyne.NewSize(100, 30))
+	EntryF2.Resize(fyne.NewSize(100, 30))
 	bt1.Resize(fyne.NewSize(70, 25))
+	//Подсказки к полям ввода /Hints for input fields
+	EntryF1.SetPlaceHolder("Угол1")
+	EntryF2.SetPlaceHolder("Угол2")
+	EntryX1.SetPlaceHolder("X1")
+	EntryX2.SetPlaceHolder("X2")
+	EntryZ1.SetPlaceHolder("Z1")
+	EntryZ2.SetPlaceHolder("Z2")
+
 	// Контейнер предназначен для вывода тех элементов которые указаны /The container is designed to display the specified elements
 	content := container.NewWithoutLayout(EntryX1, EntryX2, EntryZ1, EntryZ2, EntryF2, EntryF1, bt1, Out)
 	myWindow.SetContent(content)
